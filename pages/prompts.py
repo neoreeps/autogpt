ae66a5d5a39dca06193adec7dae7f40c9c92e07c
Kenny Speer <kenny.speer@gmail.com>
2023-05-25 09:13:31 +0200

    Add prompt configuration.
    Signed-off-by: Kenny Speer <kenny.speer@gmail.com>

---
+++ pages/prompts.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# System Prompts")
+st.sidebar.markdown("# System Prompts")
+st.sidebar.markdown("Use this page to configure the system prompts for the types of content.")
