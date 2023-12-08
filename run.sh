b279237446068d97f74b725a25e4a48d8ef9f688
Kenny Speer <kenny@reeps.io>
2023-12-07 19:59:10 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export STREAMLIT_SERVER_PORT=8081
-export OPENAI_API_KEY='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'
+export OPENAI_API_KEY='sk-yourkey'
 streamlit run main.py
